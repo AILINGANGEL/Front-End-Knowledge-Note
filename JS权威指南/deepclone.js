63fbe37a2360e6c88b4aa917755aedcf8e008969
ailingangel <angel20081018@163.com>
2019-04-10 16:03:12 +0800

    添加测试deepclone的测试用例

--- JS权威指南/deepclone.js
+++ JS权威指南/deepclone.js
@@ -23,8 +23,14 @@ var test = {
     b: 'test',
     c: 1,
     d: [1, 2],
+    e: {
+        a: 1
+    },
     now: new Date(),
     reg: /[0-9]/
 }
 
 var testCopy = deepClone(test);
+testCopy.e.a = 'a';
+console.log(testCopy);
+console.log(test);
