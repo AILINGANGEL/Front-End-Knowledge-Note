353424008cdda6f41af08df91ec1207307de1dc0
ailingangel <angel20081018@163.com>
2019-05-06 19:16:28 +0800

    Promise.finally

--- 问题/code/异步和性能/Promise.finnally.js
+++ 问题/code/异步和性能/Promise.finnally.js
@@ -1,10 +1,13 @@
 Promise.prototype.finnaly = function(cb) {
     return this.then(function(value) {
-        cb();
-        return value;
+        //cb的结果也可能是异步的需要Promise.resolve
+        return Promise.resolve(cb()).then(function() {
+            return value;
+        });
     }, function(error) {
-        cb();
-        throw error;
+        return Promise.resolve(cb()).then(function() {
+            throw error;
+        });
     });
 }
 
